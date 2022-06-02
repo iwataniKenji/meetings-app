497c3a6d0bc1bd377f9fa792670f67c8c708ed08
Kleverson Kenji <kleversonkenji@gmail.com>
2022-06-02 10:09:15 -0300

    context usage to control user auth, authentication functionality and layout minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,61 @@
+import { createContext, useState } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import { Routes } from "react-router";
+
 import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
 
 import "./styles/global.css";
+import { auth } from "./services/firebase";
+import firebase from "firebase/compat/app";
+
+interface UserProps {
+  id: string;
+  name: string;
+}
+
+interface AuthContextType {
+  user: UserProps | undefined;
+  signInWithGoogle: () => Promise<void>;
+}
+
+export const AuthContext = createContext({} as AuthContextType);
 
 function App() {
-  return <Home />;
+  const [user, setUser] = useState<UserProps>();
+
+  async function signInWithGoogle() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    const result = await auth.signInWithPopup(provider);
+
+    // se autenticação deu certo
+    if (result.user) {
+      const { displayName, uid } = result.user;
+
+      // se usuário não possuir nome para display = erro
+      if (!displayName) {
+        throw new Error("Informação insuficiente");
+      }
+
+      // se usuário possuir nome para display = configurar usuário
+      setUser({
+        id: uid,
+        name: displayName,
+      });
+    }
+  }
+
+  return (
+    <BrowserRouter>
+      <AuthContext.Provider value={{ user, signInWithGoogle }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/rooms/new" element={<NewRoom />} />
+        </Routes>
+      </AuthContext.Provider>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,33 @@
+import { useNavigate } from "react-router-dom";
+
+import { useContext } from "react";
+import { AuthContext } from "../App";
+
 import "../styles/auth.scss";
 
 export function Home() {
+  const history = useNavigate();
+  const { user, signInWithGoogle } = useContext(AuthContext);
+
+  async function handleCreateRoom() {
+    // se não estiver autenticado -> sign in
+    if (!user) {
+      await signInWithGoogle();
+    }
+
+    // se estiver autenticado -> redirecionar 
+    history("/rooms/new");
+  }
+
   return (
     <div id="auth-section">
-      <button className="create-room-btn">Crie sua assembleia com o Google</button>
+      <button onClick={handleCreateRoom} className="auth-button">
+        Crie sua assembleia com o Google
+      </button>
       <div>ou entre em uma já existente</div>
       <form>
         <input type="text" placeholder="Digite o código" />
-        <button className="join-room-btn" type="submit">
+        <button type="submit">
           Entrar na assembleia
         </button>
       </form>
